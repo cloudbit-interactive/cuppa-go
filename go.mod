6b3d4330aafa870bfbd41eae5eacd93848bc2c15
Tufik Chediak <tufik2@gmail.com>
2021-08-04 15:37:42 -0400

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/cloudbit-interactive/cuppago
 
 go 1.15
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/google/uuid v1.3.0
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
+)
--- strings.go
+++ strings.go
@@ -3,89 +3,108 @@
 > go get golang.org/x/crypto/bcrypt
 */
 
-package utils
+package cuppago
 
 import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"github.com/google/uuid"
-	"golang.org/x/crypto/bcrypt"
 	"regexp"
 	"strings"
 	"unicode"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
-func PathData(path string)map[string]interface{}{
+func PathData(path string) map[string]interface{} {
 	var result = make(map[string]interface{})
 	// get base
-		var base = path
-		if(strings.Index(base, "?") != -1){ base = base[0:strings.Index(base, "?")] }
-		if(strings.Index(base, "#") != -1){ base = base[0:strings.Index(base, "#")] }
-		result["base"] = base;
-		result["baseArray"] = strings.Split(base,"/")
+	var base = path
+	if strings.Index(base, "?") != -1 {
+		base = base[0:strings.Index(base, "?")]
+	}
+	if strings.Index(base, "#") != -1 {
+		base = base[0:strings.Index(base, "#")]
+	}
+	result["base"] = base
+	result["baseArray"] = strings.Split(base, "/")
 	// domain
-		var domain = strings.Replace(base, "https://", "", -1)
-			domain = strings.Replace(domain, "http://", "", -1)
-			domain = domain[0: strings.Index(domain,"/")]
-			result["domain"] = domain
+	var domain = strings.Replace(base, "https://", "", -1)
+	domain = strings.Replace(domain, "http://", "", -1)
+	domain = domain[0:strings.Index(domain, "/")]
+	result["domain"] = domain
 	// protocol
-		result["protocol"] = "http"
-		if(strings.Index(path, "https://") != -1){ result["protocol"] = "https" }
+	result["protocol"] = "http"
+	if strings.Index(path, "https://") != -1 {
+		result["protocol"] = "https"
+	}
 	// data
-		var dataStr = path
-		if(strings.Index(dataStr, "?") != -1 || strings.Index(dataStr, "#") != -1) {
-			if(strings.Index(dataStr, "?") != -1){ dataStr = dataStr[strings.Index(dataStr, "?")+1: len(dataStr)] }
-			if(strings.Index(dataStr, "#") != -1){ dataStr = dataStr[strings.Index(dataStr, "#")+1: len(dataStr)] }
-			var data = make(map[string]interface{})
-			var dataArray = strings.Split(dataStr,"&")
-			for i := 0; i < len(dataArray); i++{
-				var parts = strings.Split(dataArray[i],"=")
-				if(parts[0] != ""){
-					if(parts[1] != ""){
-						data[parts[0]] = parts[1]
-					}else{
-						data[parts[0]] = ""
-					}
+	var dataStr = path
+	if strings.Index(dataStr, "?") != -1 || strings.Index(dataStr, "#") != -1 {
+		if strings.Index(dataStr, "?") != -1 {
+			dataStr = dataStr[strings.Index(dataStr, "?")+1 : len(dataStr)]
+		}
+		if strings.Index(dataStr, "#") != -1 {
+			dataStr = dataStr[strings.Index(dataStr, "#")+1 : len(dataStr)]
+		}
+		var data = make(map[string]interface{})
+		var dataArray = strings.Split(dataStr, "&")
+		for i := 0; i < len(dataArray); i++ {
+			var parts = strings.Split(dataArray[i], "=")
+			if parts[0] != "" {
+				if parts[1] != "" {
+					data[parts[0]] = parts[1]
+				} else {
+					data[parts[0]] = ""
 				}
 			}
-			result["data"] = data
 		}
+		result["data"] = data
+	}
 	return result
 }
 
-func JSONEncode(value interface{}, base64Encode bool)string{
+func JSONEncode(value interface{}, base64Encode bool) string {
 	result := ""
 	bites, err := json.Marshal(value)
-	if err == nil { result = string(bites) }
-	if base64Encode == true { result = Base64Encode(result) }
-	return result;
+	if err == nil {
+		result = string(bites)
+	}
+	if base64Encode == true {
+		result = Base64Encode(result)
+	}
+	return result
 }
 
 /*
-		data := utils.JSONDecode(string, false)
- */
-func JSONDecode(value string, base64Decode bool)interface{}{
-	if base64Decode == true { value = Base64Decode(value) }
+	data := utils.JSONDecode(string, false)
+*/
+func JSONDecode(value string, base64Decode bool) interface{} {
+	if base64Decode == true {
+		value = Base64Decode(value)
+	}
 	var result interface{}
 	json.Unmarshal([]byte(value), &result)
 	return result
 }
 
-func Base64Encode(string string)string{
+func Base64Encode(string string) string {
 	result := base64.StdEncoding.EncodeToString([]byte(string))
 	return result
 }
 
-func Base64Decode(value string)string{
+func Base64Decode(value string) string {
 	result := ""
 	data, err := base64.StdEncoding.DecodeString(value)
-	if err == nil { result = string(data) }
+	if err == nil {
+		result = string(data)
+	}
 	return result
 }
 
 /* Parse any value to String */
-func String(value interface{}) string{
+func String(value interface{}) string {
 	result := fmt.Sprint(value)
 	if result == "<nil>" {
 		result = ""
@@ -106,22 +125,24 @@ func Hash(value string) string {
 func CompareHash(hash string, value string) bool {
 	byteHash := []byte(hash)
 	err := bcrypt.CompareHashAndPassword(byteHash, []byte(value))
-	if err != nil { return false }
+	if err != nil {
+		return false
+	}
 	return true
 }
 
-func InterfaceToString(value interface{})string{
+func InterfaceToString(value interface{}) string {
 	str := fmt.Sprintf("%v", value)
 	return str
 }
 
-func ReplaceNotCase(value string, search string, replace string)string{
+func ReplaceNotCase(value string, search string, replace string) string {
 	return strings.Trim(regexp.MustCompile(`(?i)`+search).ReplaceAllString(value, replace), " ")
 }
 
 func Camelize(in string) string {
-	in = strings.ReplaceAll(in, " ", "_");
-	in = strings.ReplaceAll(in, "-", "_");
+	in = strings.ReplaceAll(in, " ", "_")
+	in = strings.ReplaceAll(in, "-", "_")
 	runes := []rune(in)
 	var out []rune
 	for i, r := range runes {
