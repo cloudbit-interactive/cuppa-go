d74bf305260f12e4ce94cd8e8c9a05f923f42af6
Tufik Chediak <tufik2@gmail.com>
2024-04-17 22:03:58 -0400

    bug fixes

--- file.go
+++ file.go
@@ -12,7 +12,7 @@ func GetRootPath() string {
 		panic(err)
 	}
 	path := filepath.Dir(ex)
-	if strings.Contains(strings.ToLower(path), "temp") == true || strings.Contains(strings.ToLower(path), "private/") == true || strings.Contains(strings.ToLower(path), "/t/") == true {
+	if strings.Contains(strings.ToLower(path), "/temp/") == true || strings.Contains(strings.ToLower(path), "/tmp/") == true || strings.Contains(strings.ToLower(path), "private/") == true || strings.Contains(strings.ToLower(path), "/t/") == true {
 		path, err := os.Getwd()
 		if err != nil {
 			panic(err)
